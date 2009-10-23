620595ba038ae847ea6f410817921082122ce8b8
Drew Fisher <kakudevel@gmail.com>
2009-10-23 15:49:16 -0700

    Made a few code cleanups to test_view.php

--- test_view.php
+++ test_view.php
@@ -35,20 +35,23 @@ if(isset($_GET['event']))
 	$connection = new Connection();
 	$eventID = $connection->validate_string($_GET['event']);
 	$query = "
-SELECT e_eventID, e_eventname, r_roomname, e_dateStart, e_dateEnd, e_eventDesc, e_panelist, e_color
+SELECT e_eventID, e_eventname, r_roomname, e_dateStart, e_dateEnd, 
+e_eventDesc, e_panelist, e_color
 FROM events, rooms
 WHERE e_eventID = $eventID AND e_roomID = r_roomID;";
 	$connection->query($query);
 	
 	if($connection->result_size() != 1)
 	{
-		echo "Invalid Event ID, please check that you are using the correct EventID.  If you are, report this to Kitsune on the Mewcon Forums via PM. <br /><br />";
+		echo "Invalid Event ID, please check that you are using the correct EventID. "
+		echo "If you are, report this to Kitsune on the Mewcon Forums via PM. <br /><br />";
 		$page->addURL("test.php","Back to event schedule.");
 		break;
 	}
 	
 	$row = $connection->fetch_row();
 	$event = new Event($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7]);
+	
 	//ok lets pass this info to the page so it can display it
 	$page->printEvent($event);
 	
@@ -61,7 +64,7 @@ WHERE e_eventID = $eventID AND e_roomID = r_roomID;";
 	}
 	else if( $username == $event->getPanelist())
 	{
-		#this is the panalist for this panel so give them access to the desc editing
+		//this is the panelist for this panel so give them access to the desc editing
 		echo "<br><br>";
 		$page->printPanelistEdit($event, $eventID);
 	}
