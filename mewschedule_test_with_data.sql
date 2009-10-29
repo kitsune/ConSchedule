97248e13edc7be181ec0c5ea1612246448e4a473
Drew Fisher <kakudevel@gmail.com>
2009-10-29 00:09:21 -0700

    Updated the sql to have a much larger number of events that also spans multiple con days. Very copy-paste-y, but it works for testing purposes.

--- mewschedule_test_with_data.sql
+++ mewschedule_test_with_data.sql
@@ -4,7 +4,7 @@
 #
 # Host: localhost (MySQL 5.1.37)
 # Database: mewschedule_test
-# Generation Time: 2009-10-24 10:47:26 -0700
+# Generation Time: 2009-10-29 00:07:38 -0700
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -32,18 +32,87 @@ CREATE TABLE `events` (
   `e_eventDesc` text NOT NULL,
   `e_panelist` tinytext NOT NULL,
   PRIMARY KEY (`e_eventID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=76 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
 INSERT INTO `events` (`e_eventID`,`e_roomID`,`e_dateStart`,`e_dateEnd`,`e_eventName`,`e_color`,`e_eventDesc`,`e_panelist`)
 VALUES
-	(1,1,'2009-12-31 12:00:00','2009-12-31 14:00:00','Test 1','#FF0000','This is a 2-hour event being held on 2009-10-20 starting at 12:00 and going till 14:00, in r_roomID 0.','Admin'),
-	(2,1,'2009-12-31 14:00:00','2009-12-31 16:30:00','another fairly long title for T2','#CCCCCC','','Kaku'),
-	(3,2,'2009-12-31 14:00:00','2009-12-31 18:00:00','Test 3','#0066FF','this is a description for test 3','Admin'),
-	(4,5,'2009-12-31 16:00:00','2009-12-31 17:30:00','yet more wasted space with Test 4','#FFCA52','Description for Test 4','Kitsune'),
-	(5,4,'2009-12-31 09:00:00','2009-12-31 10:00:00','obscenely and gratuitously long title involving a sql test for T5','#00FF00','','Admin'),
-	(6,3,'2009-12-31 22:00:00','2010-01-01 01:30:00','Late Night Test 6','#0066FF','Test to make sure going from previous day to next works as expected.','Kaku');
+	(1,1,'2009-12-31 12:00:00','2009-12-31 14:00:00','Event Test 1: The Beginning.','#FF0000','In the beginning, Japan created Anime while the West created Fantasy/Sci-Fi, and it wad decent.\\nThen Admin descended upon the world and gave us MEWcon, and it was awesome.','Admin'),
+	(2,1,'2009-12-31 14:00:00','2009-12-31 16:30:00','Event Test 2, with a fairly long name, but no description.','#CCCCCC','','Kaku'),
+	(3,2,'2009-12-31 14:00:00','2009-12-31 18:00:00','Event Test 3: Revenge of the Event Test.','#0066FF','You remember him from Events 1 and 2, now Event Test returns in a cinematographic exercise in unbelievability.','Admin'),
+	(4,5,'2009-12-31 16:00:00','2009-12-31 17:30:00','Event Test 4, which also wastes some space with its name.','#FFCA52','A non-witty description for Event Test 4.','Kitsune'),
+	(5,4,'2009-12-31 09:00:00','2009-12-31 12:30:00','Event Test 5, which has an obscenely long name and a less equally long description. That is to say, it has no description at all.','#00FF00','','Admin'),
+	(6,3,'2009-12-31 22:00:00','2010-01-01 01:30:00','Event Test 6: For the Night Owls.','#0066FF','Prowl the night with this Event. AKA: The test that makes sure pre-midnight to post-midnight events work as expected.','Kaku'),
+	(7,7,'2009-12-31 08:00:00','2009-12-31 12:00:00','Event Test 7: Gamer Edition.','#FF00FF','Join Event on an epic journey to save the princess, who fails to put out just like the last 6 games!','Legoman'),
+	(8,6,'2009-12-31 08:00:00','2009-12-31 15:00:00','Event Test 8: Pocket Protector Power (P3).','#CCCCCC','Don\'t know where that book is, mate. There\'s a lot of them about!','Kitsune'),
+	(9,2,'2009-12-31 10:00:00','2009-12-31 13:30:00','Event Test 9','#777777','Go Go Gadget Event Test!','Kai'),
+	(10,3,'2009-12-31 08:30:00','2009-12-31 09:00:00','Event Test 10','#7777CC','','Admin'),
+	(11,4,'2009-12-31 08:00:00','2009-12-31 11:30:00','Event Test 11','#CC77CC','','TOFU'),
+	(12,3,'2009-12-31 09:30:00','2009-12-31 10:30:00','Event Test 12','#FFFF00','','Legoman'),
+	(13,7,'2009-12-31 12:30:00','2009-12-31 15:30:00','Event Test 13','#5A5A5A','This little Event went to market; this little Event stayed home; and this little Event went \"OMG stop poking my kidneys!\"','Kaku'),
+	(14,5,'2009-12-31 08:00:00','2009-12-31 11:00:00','Event Test 14','#CCFFCC','',''),
+	(15,5,'2009-12-31 11:00:00','2009-12-31 12:30:00','Event Test 15','#FFCCCC','',''),
+	(16,3,'2009-12-31 11:00:00','2009-12-31 12:30:00','Event Test 16','#CCCCFF','',''),
+	(17,3,'2009-12-31 13:00:00','2009-12-31 15:30:00','Event Test 17','#CCFFCC','',''),
+	(18,4,'2009-12-31 11:30:00','2009-12-31 15:00:00','Event Test 18','#00CC00','',''),
+	(19,1,'2009-12-31 08:00:00','2009-12-31 11:30:00','Event Test 19','#CC0000','',''),
+	(20,5,'2009-12-31 13:00:00','2009-12-31 15:00:00','Event Test 20','#0000CC','',''),
+	(21,2,'2009-12-31 08:30:00','2009-12-31 09:30:00','Event Test 21','#CCCC00','',''),
+	(22,6,'2009-12-31 16:30:00','2009-12-31 18:30:00','Event Test 22','#00CCCC','',''),
+	(23,7,'2009-12-31 16:30:00','2009-12-31 20:00:00','Event Test 23','#CC00CC','',''),
+	(24,1,'2009-12-31 17:00:00','2009-12-31 19:30:00','Event Test 24','#C0C0C0','',''),
+	(25,3,'2009-12-31 17:30:00','2009-12-31 19:00:00','Event Test 25','#CC99FF','',''),
+	(26,1,'2010-01-01 12:00:00','2010-01-01 14:00:00','D2 Event Test 1: The Beginning.','#FF0000','In the beginning, Japan created Anime while the West created Fantasy/Sci-Fi, and it wad decent.\\nThen Admin descended upon the world and gave us MEWcon, and it was awesome.','Admin'),
+	(27,1,'2010-01-01 14:00:00','2010-01-01 16:30:00','D2 Event Test 2, with a fairly long name, but no description.','#CCCCCC','','Kaku'),
+	(28,2,'2010-01-01 14:00:00','2010-01-01 18:00:00','D2 Event Test 3: Revenge of the D2 Event Test.','#0066FF','You remember him from Events 1 and 2, now D2 Event Test returns in a cinematographic exercise in unbelievability.','Admin'),
+	(29,5,'2010-01-01 16:00:00','2010-01-01 17:30:00','D2 Event Test 4, which also wastes some space with its name.','#FFCA52','A non-witty description for D2 Event Test 4.','Kitsune'),
+	(30,4,'2010-01-01 09:00:00','2010-01-01 12:30:00','D2 Event Test 5, which has an obscenely long name and a less equally long description. That is to say, it has no description at all.','#00FF00','','Admin'),
+	(31,3,'2010-01-01 22:00:00','2010-01-02 01:30:00','D2 Event Test 6: For the Night Owls.','#0066FF','Prowl the night with this Event. AKA: The test that makes sure pre-midnight to post-midnight events work as expected.','Kaku'),
+	(32,7,'2010-01-01 08:00:00','2010-01-01 12:00:00','D2 Event Test 7: Gamer Edition.','#FF00FF','Join Event on an epic journey to save the princess, who fails to put out just like the last 6 games!','Legoman'),
+	(33,6,'2010-01-01 08:00:00','2010-01-01 15:00:00','D2 Event Test 8: Pocket Protector Power (P3).','#CCCCCC','Don\'t know where that book is, mate. There\'s a lot of them about!','Kitsune'),
+	(34,2,'2010-01-01 10:00:00','2010-01-01 13:30:00','D2 Event Test 9','#777777','Go Go Gadget D2 Event Test!','Kai'),
+	(35,3,'2010-01-01 08:30:00','2010-01-01 09:00:00','D2 Event Test 10','#7777CC','','Admin'),
+	(36,4,'2010-01-01 08:00:00','2010-01-01 11:30:00','D2 Event Test 11','#CC77CC','','TOFU'),
+	(37,3,'2010-01-01 09:30:00','2010-01-01 10:30:00','D2 Event Test 12','#FFFF00','','Legoman'),
+	(38,7,'2010-01-01 12:30:00','2010-01-01 15:30:00','D2 Event Test 13','#5A5A5A','This little Event went to market; this little Event stayed home; and this little Event went \"OMG stop poking my kidneys!\"','Kaku'),
+	(39,5,'2010-01-01 08:00:00','2010-01-01 11:00:00','D2 Event Test 14','#CCFFCC','',''),
+	(40,5,'2010-01-01 11:00:00','2010-01-01 12:30:00','D2 Event Test 15','#FFCCCC','',''),
+	(41,3,'2010-01-01 11:00:00','2010-01-01 12:30:00','D2 Event Test 16','#CCCCFF','',''),
+	(42,3,'2010-01-01 13:00:00','2010-01-01 15:30:00','D2 Event Test 17','#CCFFCC','',''),
+	(43,4,'2010-01-01 11:30:00','2010-01-01 15:00:00','D2 Event Test 18','#00CC00','',''),
+	(44,1,'2010-01-01 08:00:00','2010-01-01 11:30:00','D2 Event Test 19','#CC0000','',''),
+	(45,5,'2010-01-01 13:00:00','2010-01-01 15:00:00','D2 Event Test 20','#0000CC','',''),
+	(46,2,'2010-01-01 08:30:00','2010-01-01 09:30:00','D2 Event Test 21','#CCCC00','',''),
+	(47,6,'2010-01-01 16:30:00','2010-01-01 18:30:00','D2 Event Test 22','#00CCCC','',''),
+	(48,7,'2010-01-01 16:30:00','2010-01-01 20:00:00','D2 Event Test 23','#CC00CC','',''),
+	(49,1,'2010-01-01 17:00:00','2010-01-01 19:30:00','D2 Event Test 24','#C0C0C0','',''),
+	(50,3,'2010-01-01 17:30:00','2010-01-01 19:00:00','D2 Event Test 25','#CC99FF','',''),
+	(51,1,'2010-01-02 12:00:00','2010-01-02 14:00:00','D3 Event Test 1: The Beginning.','#FF0000','In the beginning, Japan created Anime while the West created Fantasy/Sci-Fi, and it wad decent.\\nThen Admin descended upon the world and gave us MEWcon, and it was awesome.','Admin'),
+	(52,1,'2010-01-02 14:00:00','2010-01-02 16:30:00','D3 Event Test 2, with a fairly long name, but no description.','#CCCCCC','','Kaku'),
+	(53,2,'2010-01-02 14:00:00','2010-01-02 18:00:00','D3 Event Test 3: Revenge of the D3 Event Test.','#0066FF','You remember him from Events 1 and 2, now D3 Event Test returns in a cinematographic exercise in unbelievability.','Admin'),
+	(54,5,'2010-01-02 16:00:00','2010-01-02 17:30:00','D3 Event Test 4, which also wastes some space with its name.','#FFCA52','A non-witty description for D3 Event Test 4.','Kitsune'),
+	(55,4,'2010-01-02 09:00:00','2010-01-02 12:30:00','D3 Event Test 5, which has an obscenely long name and a less equally long description. That is to say, it has no description at all.','#00FF00','','Admin'),
+	(56,3,'2010-01-02 22:00:00','2010-01-03 00:00:00','D3 Event Test 6: For the Night Owls.','#0066FF','Prowl the night with this Event. AKA: The test that makes sure pre-midnight to post-midnight events work as expected.','Kaku'),
+	(57,7,'2010-01-02 08:00:00','2010-01-02 12:00:00','D3 Event Test 7: Gamer Edition.','#FF00FF','Join Event on an epic journey to save the princess, who fails to put out just like the last 6 games!','Legoman'),
+	(58,6,'2010-01-02 08:00:00','2010-01-02 15:00:00','D3 Event Test 8: Pocket Protector Power (P3).','#CCCCCC','Don\'t know where that book is, mate. There\'s a lot of them about!','Kitsune'),
+	(59,2,'2010-01-02 10:00:00','2010-01-02 13:30:00','D3 Event Test 9','#777777','Go Go Gadget D3 Event Test!','Kai'),
+	(60,3,'2010-01-02 08:30:00','2010-01-02 09:00:00','D3 Event Test 10','#7777CC','','Admin'),
+	(61,4,'2010-01-02 08:00:00','2010-01-02 11:30:00','D3 Event Test 11','#CC77CC','','TOFU'),
+	(62,3,'2010-01-02 09:30:00','2010-01-02 10:30:00','D3 Event Test 12','#FFFF00','','Legoman'),
+	(63,7,'2010-01-02 12:30:00','2010-01-02 15:30:00','D3 Event Test 13','#5A5A5A','This little Event went to market; this little Event stayed home; and this little Event went \"OMG stop poking my kidneys!\"','Kaku'),
+	(64,5,'2010-01-02 08:00:00','2010-01-02 11:00:00','D3 Event Test 14','#CCFFCC','',''),
+	(65,5,'2010-01-02 11:00:00','2010-01-02 12:30:00','D3 Event Test 15','#FFCCCC','',''),
+	(66,3,'2010-01-02 11:00:00','2010-01-02 12:30:00','D3 Event Test 16','#CCCCFF','',''),
+	(67,3,'2010-01-02 13:00:00','2010-01-02 15:30:00','D3 Event Test 17','#CCFFCC','',''),
+	(68,4,'2010-01-02 11:30:00','2010-01-02 15:00:00','D3 Event Test 18','#00CC00','',''),
+	(69,1,'2010-01-02 08:00:00','2010-01-02 11:30:00','D3 Event Test 19','#CC0000','',''),
+	(70,5,'2010-01-02 13:00:00','2010-01-02 15:00:00','D3 Event Test 20','#0000CC','',''),
+	(71,2,'2010-01-02 08:30:00','2010-01-02 09:30:00','D3 Event Test 21','#CCCC00','',''),
+	(72,6,'2010-01-02 16:30:00','2010-01-02 18:30:00','D3 Event Test 22','#00CCCC','',''),
+	(73,7,'2010-01-02 16:30:00','2010-01-02 20:00:00','D3 Event Test 23','#CC00CC','',''),
+	(74,1,'2010-01-02 17:00:00','2010-01-02 19:30:00','D3 Event Test 24','#C0C0C0','',''),
+	(75,3,'2010-01-02 17:30:00','2010-01-02 19:00:00','D3 Event Test 25','#CC99FF','','');
 
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
